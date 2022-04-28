4b1693376890c5e2381458e104d19544e015da12
EL225 <ericleonmora99@gmail.com>
2022-04-28 03:07:50 +0000

    ejercicio_terminado

--- src/app.js
+++ src/app.js
@@ -9,4 +9,16 @@ window.onload = function() {
   var pronoun = ["the", "our"];
   var adj = ["great", "big"];
   var noun = ["jogger", "racoon"];
+  function randnum() {
+    let num = Math.floor(Math.random() * 2);
+    return num;
+  }
+  function generador() {
+    let domain = pronoun[randnum()] + adj[randnum()] + noun[randnum()] + ".com";
+    return domain;
+  }
+
+  for (var i = 0; i < 9; i++) {
+    console.log(generador());
+  }
 };
